d3ce9e3f93cf5a8de9a4245467ab1b8a2f17f288
nyang5504 <ninayang3.14@gmail.com>
2022-02-05 19:23:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.simpletodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import org.apache.commons.io.FileUtils
+import java.io.File
+import java.io.IOException
+import java.nio.charset.Charset
+
+class MainActivity : AppCompatActivity() {
+
+    var listOfTasks = mutableListOf<String>()
+    lateinit var adapter: TaskItemAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val onLongClickListener = object: TaskItemAdapter.OnLongClickListener {
+            override fun onItemLongClicked(position: Int) {
+                listOfTasks.removeAt(position)
+                adapter.notifyDataSetChanged()
+
+                saveItems()
+            }
+
+        }
+
+//        findViewById<Button>(R.id.button).setOnClickListener {
+//            Log.i("Hi", "Hello, World!")
+//        }
+
+        loadItems()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        // Create adapter passing in the sample user data
+        adapter = TaskItemAdapter(listOfTasks, onLongClickListener)
+        // Attach the adapter to the recyclerview to populate items
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val inputTextField = findViewById<EditText>(R.id.addTaskField)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val userInputtedTask = inputTextField.text.toString()
+
+            listOfTasks.add(userInputtedTask)
+
+            adapter.notifyItemInserted(listOfTasks.size - 1)
+            inputTextField.setText("")
+
+            saveItems()
+        }
+    }
+
+    // Get the file we need
+    fun getDataFile() : File {
+        return File(filesDir, "data.txt")
+    }
+
+    // Load the items by reading every line in the data file
+    fun loadItems() {
+        try {
+            listOfTasks = FileUtils.readLines(getDataFile(), Charset.defaultCharset())
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+        }
+
+    }
+
+    // Save items by writing them into our data file
+    fun saveItems() {
+        try {
+            FileUtils.writeLines(getDataFile(), listOfTasks)
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+        }
+    }
+}
